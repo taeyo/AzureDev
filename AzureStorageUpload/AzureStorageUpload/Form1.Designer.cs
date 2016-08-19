237b340ff5be7a162488dcbf92c1236841addb87
Taeyo <taeyokim@gmail.com>
2016-08-19 11:16:26 +0900

    일부 결과 텍스트 안보이는 문제 해결

--- AzureStorageUpload/AzureStorageUpload/Form1.Designer.cs
+++ AzureStorageUpload/AzureStorageUpload/Form1.Designer.cs
@@ -276,9 +276,9 @@
             this.Mps.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Mps.Location = new System.Drawing.Point(302, 312);
             this.Mps.Name = "Mps";
-            this.Mps.Size = new System.Drawing.Size(70, 23);
+            this.Mps.Size = new System.Drawing.Size(130, 23);
             this.Mps.TabIndex = 1;
-            this.Mps.Text = "( 0 M/s)";
+            this.Mps.Text = "( 0 M/s)               ";
             // 
             // label7
             // 
