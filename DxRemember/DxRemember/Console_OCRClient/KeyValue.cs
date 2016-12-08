ac731c7e22074e66c6b1e2a6b40ef178538aea1f
taeyo <taeyokim@gmail.com>
2016-12-08 13:43:38 +0900

    namespace fixed

---
+++ DxRemember/DxRemember/Console_OCRClient/KeyValue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Console_OCRClient
+{
+    public class KeyValue
+    {
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+    }
+}
--- DxRemember/DxRemember/Console_OCRClient/OCRClient.cs
+++ DxRemember/DxRemember/Console_OCRClient/OCRClient.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.CognitiveServices.ContentModerator;
-using Microsoft.CognitiveServices.ContentModerator.Contract.Image;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
